35c1ab35bf5d4fb92a351bce554cb34a42f29b89
HassanRaza293 <hassan.raza@clarisync.com>
2025-12-16 14:01:01 +0500

    iterations

--- basics/iterations/for_loop.js
+++ basics/iterations/for_loop.js
@@ -1,19 +1,19 @@
 for(let i = 50; i > 0; i--){
     console.log("Iteration number: " + i);
     if(i==20){
-        break;
+        break;   // adding the break statement here
     }
 }
 for(let i = 0; i<20; i++){
     console.log("Iteration number: " + i);
     if(i==10){
         console.log("Skipping iteration 20");
-        continue;
+        continue; // adding the continue statement here
     }
 }
 
 for(let i=0; i<=10; i++){
-    for(let j=0; j<=10; j++){
+    for(let j=0; j<=10; j++){   // nested loop
         console.log(`i: ${i}, j: ${j}`);
     }
 
@@ -23,7 +23,7 @@ for(let i=0; i<=10; i++){
 let myArray = ['hassan', 'ahmed', 'sara', 'lina'];
 
 for(let i = 0; i < myArray.length; i++){
-    console.log(`Index: ${i}, Value: ${myArray[i]}`);
+    console.log(`Index: ${i}, Value: ${myArray[i]}`);  //iteration over array
 
 
 }

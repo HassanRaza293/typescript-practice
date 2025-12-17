de3a32ab853b0d01cc7453dd143a889a1cbdeac2
HassanRaza293 <hassan.raza@clarisync.com>
2025-12-17 10:57:39 +0500

    functions

---
+++ basics/functions/1_functions.js
@@ -0,0 +1,16 @@
+// function addTwoNumbers(number1, number2) {
+//     console.log(number1 + number2);
+//     return number1 + number2;
+// }
+
+// const result = addTwoNumbers(5, 10);
+// console.log("Result is: " + result);
+
+
+
+
+function calculatedCartPrice(...arr) {
+    return arr;
+}
+
+console.log(calculatedCartPrice(100, 200, 300, 400, 500));

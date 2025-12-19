d28dae08366b5f8f499d82a16851c63d8b18ee2a
HassanRaza293 <hassan.raza@clarisync.com>
2025-12-19 11:57:44 +0500

    getter and setters

---
+++ basics/oop/setter_getter.js
@@ -0,0 +1,27 @@
+class User{
+    constructor(email, password){
+        this.email = email;
+        this.password = password;
+    }
+
+    set password(password){
+        this._password = password;
+    }
+
+    get email(){
+        return this._email.toUpperCase();
+    }
+
+    set email(email){
+        this._email = email;
+    }
+
+    get password(){
+        return `${this._password}hitest`;
+    }
+}
+
+const hassan = new User("hassan@example.com", "password123");
+console.log(hassan.password); 
+
+

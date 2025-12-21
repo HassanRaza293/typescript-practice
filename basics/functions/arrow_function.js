25b7d05597fed465b3b306f96a0e8e0d809dd054
HassanRaza293 <hassan.raza@clarisync.com>
2025-12-21 15:04:55 +0500

    arrow-functions-added

---
+++ basics/functions/arrow_function.js
@@ -0,0 +1,12 @@
+const addNum = (num1,num2)=>{  //explicit way of writing
+    return num1+num2;
+}
+const addtion =(num1,num2)=>(num1+num2) //implicit way of writing 
+
+const getNames=(username1,username2)=>({username1:username1, username2:username2})
+//whenever we write curly brackets in functions we need to write return statement
+
+console.log(addNum(4,3));
+console.log(addtion(5,2));
+console.log(getNames("Hassan","Ali"));
+
